28023187155524f3071e4fe2646a46267bf33127
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-11-22 18:47:04 -0300

    Routes for signed and not signed user.

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
 import { store, persistor } from '~/store';
-import Routes from '~/routes';
+
+import App from '~/App';
 
 const src = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import PropTypes from 'prop-types';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+import Home from '~/pages/Home';
 
 const Stack = createStackNavigator();
 
-const routes = () => {
+const Routes = ({ signed }) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignIn" headerMode={false}>
+      <Stack.Navigator
+        initialRouteName={signed ? 'Home' : 'SignIn'}
+        headerMode="none"
+      >
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default routes;
+export default Routes;
+
+Routes.propTypes = {
+  signed: PropTypes.bool,
+};
+
+Routes.defaultProps = {
+  signed: false,
+};
