df1e9cae967d1ad4c2cada554192fb58a1049f86
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-11-29 19:11:32 -0300

    Navigation buttons with icons.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
+
+const dashBoardIcon = ({ color, size }) => (
+  <Icon name="event" color={color} size={size} />
+);
+
+const profileIcon = ({ color, size }) => (
+  <Icon name="person" color={color} size={size} />
+);
 
 const Tab = createBottomTabNavigator();
 
 const Home = () => {
   return (
-    <Tab.Navigator initialRouteName="Dashboard">
-      <Tab.Screen name="Dashboard" component={Dashboard} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      initialRouteName="Dashboard"
+      tabBarOptions={{
+        activeTintColor: '#fff',
+        inactiveTintColor: 'rgba(255,255,255,0.6)',
+        style: {
+          backgroundColor: '#8d41a8',
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{ tabBarIcon: dashBoardIcon }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{ tabBarIcon: profileIcon }}
+      />
     </Tab.Navigator>
   );
 };
