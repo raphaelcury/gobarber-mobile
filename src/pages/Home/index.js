4232ad163bb6cafd9cf3334a49e84cbffd182106
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-11-29 18:38:31 -0300

    Profile component and route.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ const Home = () => {
   return (
     <Tab.Navigator initialRouteName="Dashboard">
       <Tab.Screen name="Dashboard" component={Dashboard} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 };
