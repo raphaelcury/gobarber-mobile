8bca461cdb56f8db63a159ead55ee21e8f54ef43
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-11-29 17:08:25 -0300

    Fixed auth flow in reactnavigation.

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import { Provider } from 'react-redux';
 
 import { store, persistor } from '~/store';
 
-import App from '~/App';
+import Routes from '~/routes';
 
 const src = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <App />
+        <Routes />
       </PersistGate>
     </Provider>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import PropTypes from 'prop-types';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
@@ -9,27 +9,23 @@ import Home from '~/pages/Home';
 
 const Stack = createStackNavigator();
 
-const Routes = ({ signed }) => {
+const Routes = () => {
+  const signed = useSelector((state) => state.auth.signed);
+  console.tron.log(`RENDERIZEI! SIGNED=${signed}`);
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={signed ? 'Home' : 'SignIn'}
-        headerMode="none"
-      >
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator headerMode="none">
+        {signed ? (
+          <Stack.Screen name="Home" component={Home} />
+        ) : (
+          <>
+            <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default Routes;
-
-Routes.propTypes = {
-  signed: PropTypes.bool,
-};
-
-Routes.defaultProps = {
-  signed: false,
-};
